a639cb4a475de68f85f2d4067c56862a8eaee95d
Wytze Bruinsma <14125919@student.hhs.nl>
2016-12-08 15:32:21 +0100

    Setup Layout

---
+++ src/WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,13 @@
+﻿// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Webapplicatie.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/WebApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace Webapplicatie
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}

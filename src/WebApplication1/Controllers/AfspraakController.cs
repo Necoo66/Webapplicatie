e2aa53c76451e74046fef79149b6487f58154301
Lockman <chinees10@hotmail.com>
2016-12-13 11:55:03 +0100

    AfspraakController

---
+++ src/WebApplication1/Controllers/AfspraakController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webapplicatie.Controllers
+{
+    public class AfspraakController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Datum()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Datum()
+        {
+            return View();
+        }
+
+        public IActionResult Tijd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Tijd()
+        {
+            return View();
+        }
+
+        public IActionResult Gegevens()
+        {
+            return View();
+        }
+
+        public IActionResult Controleer()
+        {
+
+        }
+    }
+}

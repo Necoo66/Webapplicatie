4a22fa7d4fc722e9a0e95c44ce0ae58e6d9624da
Necati <Necati@NECATILENOVO>
2016-12-13 15:57:20 +0100

    Gebruiker model Nog niet af

---
+++ src/WebApplication1/Models/Gebruiker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webapplicatie.Models
+{
+    public class Gebruiker
+    {
+        [Key]
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public String naam { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Trouwdatum { get; set; }
+
+    }
+}

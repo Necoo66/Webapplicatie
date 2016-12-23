ac25bedcd7049e62da793da03c49817e618d8524
Lockman <chinees10@hotmail.com>
2016-12-23 01:22:39 +0100

    Stap01a Stap01b

--- HoneymoonShop/src/HoneymoonShop/wwwroot/js/DatePicker.js
+++ HoneymoonShop/src/HoneymoonShop/wwwroot/js/DatePicker.js
@@ -47,3 +47,44 @@ function toDate(sqlDate) {
     var from = ("" + sqlDate).substr(0, 10).split('-');
     return from[2] + "-" + from[1] + "-" + from[0];
 }
+
+/*TOGGLE*/
+function toggleVisibility(id) {
+    var e = document.getElementById(id);
+    if (e.style.display === "none") {
+        e.style.display = "none";
+    } else {
+        e.style.display = "block";
+    }
+}
+
+function toggleSelecteerTijd(id1, id2, id3)
+{
+   // alert("message alert");
+    var e1 = document.getElementById(id1);
+    var e2 = document.getElementById(id2);
+    var e3 = document.getElementById(id3);
+
+    if(e1.style.display === "block")
+    {
+        e1.style.display = "none"
+        e2.style.display = "block"
+        e3.style.display = "block"
+        
+    }
+    else {
+        e1.style.display = "block"
+        e2.style.display = "none"
+        e3.style.display = "none"
+    }
+
+}
+
+function toggleSelecteerTijdstip(id1, id2, id3)
+{
+    var e1 = document.getElementById(id1);
+    var e2 = document.getElementById(id2);
+    var e3 = document.getElementById(id3);
+
+
+}

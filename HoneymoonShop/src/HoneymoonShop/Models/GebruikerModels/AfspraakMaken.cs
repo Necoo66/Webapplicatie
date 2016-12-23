2393649c153cb44dff57a728fdb657134ec24d9f
Wytze Bruinsma <14125919@student.hhs.nl>
2016-12-23 12:07:42 +0100

    update controller

--- HoneymoonShop/src/HoneymoonShop/Models/GebruikerModels/AfspraakMaken.cs
+++ HoneymoonShop/src/HoneymoonShop/Models/GebruikerModels/AfspraakMaken.cs
@@ -4,5 +4,11 @@
     {
         public Afspraak Afspraak { get; set; }
         public Gebruiker Gebruiker { get; set; }
+
+        public AfspraakMaken()
+        {
+            Afspraak = new Afspraak();
+            Gebruiker = new Gebruiker();
+        }
     }
 }

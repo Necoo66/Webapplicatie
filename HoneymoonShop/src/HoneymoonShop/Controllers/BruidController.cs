d658095d076d86f5a5d17106952e7965230f4a82
Rahiem <Rahiem@RAHIEM-HP>
2017-01-19 11:45:09 +0100

    dropdown list fixed

--- HoneymoonShop/src/HoneymoonShop/Controllers/BruidController.cs
+++ HoneymoonShop/src/HoneymoonShop/Controllers/BruidController.cs
@@ -61,8 +61,8 @@ namespace HoneymoonShop.Controllers
 
             /*sorteren*/
             producten = sorteren(filterSelectie, producten);
-            /*paginanummering*/
 
+            /*paginanummering*/
             paginanummering(filterSelectie, producten);
 
             var limitedProducts = producten.Skip((filterSelectie.Paginanummer - 1) * filterSelectie.AantalTonen).Take(filterSelectie.AantalTonen).ToList();
